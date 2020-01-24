6f5312eec3c59149716f8e02005791784d429bc9
Charlie <porthose.cjsmo@gmail.com>
2020-01-24 11:19:06 -0800

    edit text

--- src/pages/mediacenter.js
+++ src/pages/mediacenter.js
@@ -1,50 +1,51 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import MediaCenterImage from "../components/MediaCenterImage"
 import MediaCenter from '../components/MediaCenterTech'
-
-
 
 const MediaCenterPage = () => (
   <Layout>
     <SEO title="MediaCenter Page" />
     <h1 id="P2">MediaCenter</h1>
     <div style={{height: 30}}></div>
-    <p>MediaCenter is written in Golang and is primarily meant to be used on the
-      <a href="/">Raspberry Pi</a>.  MediaCenter is a mashup of two other projects
-      MovieGo and TVGo.  MediaCenter was written to solve three main problems.
+    <p>MediaCenter is written in Golang and is primarily meant to be used on
+      the <a href="/">Raspberry Pi</a>.  MediaCenter is a mashup of two other
+      projects MovieGo and TVGo.  MediaCenter was written to solve three main
+      problems.
     </p>
     <div style={{maxWidth: 400, margin: "auto"}}>
       <MediaCenterImage />
     </div>
     <div style={{height: 30}}></div>
-    <p>Problem one is the inherent limited storage capability of the Raspberry Pi, 
-      depending on the size of the SD card used that could only be 64GB.  I have around
-      2TB of movies and music setting on my NAS so some type of streaming solution is 
-      needed.  
+    <p>
+      <h4>Problem 1</h4>
+      The inherent limited storage capability of the Raspberry Pi, 
+      depending on the size of the SD card used that could only be 128GB.
+      But with the help of a USB expansion board or "hat" that allows you
+      to bolt on a HHD or SSD of your choice.  This basically turns the PI
+      into a NAS on steroids.  I have around 2TB of movies being served up
+      by MediaCenter.
     </p>
-    <p>Problem two would be the crappy video playback from most if not all the 
-      video players out there.  Omxplayer is a video player that was written in C++ 
-      especially for the Raspberry Pi's  Broadcom VideoCore chipset with near DVD video 
-      quality playback in my opinion. Problem three, my poor TV is dumber than a box of 
-      rocks and has no smart capabilities, this is where the Raspberry Pi comes in.  It 
-      is a cheap way of turning my dumb TV into a smarter one.  
-      
-      
-      
-      MovieGo web front end and
-
-      organizes your movies while the backend written in golang
-      serve up your movies.  MovieGo can be accessed from any device that has a web browser.
-      I find that an old phone can be used as a MovieGo remote control.
-
-      is a proof of concept application for MovieGo.  I
-      MovieTime so I would have a clear road map on what would and wouldnt work before
-      attempting to write MovieGo.
-
+    <p>
+      <h4>Problem 2</h4>
+      Most CPU based video players have a hard time on the Raspberry Pi however
+      Omxplayer is a video player that was written in C++ especially for the 
+      Raspberry Pi's  Broadcom VideoCore chipset with near DVD video quality 
+      playback in my opinion. 
+    </p>
+    <p>
+      <h4>Problem 3</h4>
+      I have one of those cheap TV's that's dumber than a box of rocks, it 
+      has no smart capabilities, this is where the Raspberry Pi comes in.  
+      It is a cheap way of turning my dumb TV into a smarter one.  
+    </p>
+    <p>
+      MediaCenter can be accessed from any device that has a web browser.
+      Currently Omxplayer playback is only supported.  Per device support
+      is not there yet.  I find that an old phone can be used as a MovieGo 
+      remote control.
     </p>
 
     <MediaCenter />
