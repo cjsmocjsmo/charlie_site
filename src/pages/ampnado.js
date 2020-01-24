7eadc3836f7e13bc4d6d2f5e05a749d8570347df
Charlie <porthose.cjsmo@gmail.com>
2020-01-23 16:59:00 -0800

    added tech used icons

--- src/pages/ampnado.js
+++ src/pages/ampnado.js
@@ -4,17 +4,53 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Image from "../components/image"
+import Amp from '../images/ampnado.png'
+
+import GOL from '../images/golang.jpg'
+import Alpine from '../images/alpine.png'
+import Docker from '../images/docker.png'
+import BS from '../images/bootstrap.png'
+import Html from '../images/html.png'
+import AWS from '../images/aws.png'
+import Heroku from '../images/heroku.jpg'
+
 
 const AmpnadoPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1 id="P2">Ampnado</h1>
-    <p> Put some stuff here</p>
-    <Image />
-    <p>Welcome to page 2</p>
+    <p>Ampnado is a MP3 organizer and music server.  Ampnado catagorizes
+your music by Album, Artist, Song, along with playlist creation and playback.  Ampnado
+is heavely dependent on id3 tags for metadata and album cover art. Ampnado has been
+written in python and utilizes the MongoDB database for data persistence.
+HTML, CSS, JQuery, JQuery-Mobile were used for the front-end.  I have
+Ampnado running on a Raspberry Pi with a 128GB microSD card.
+    </p>
+    <div style={{maxWidth: 400, margin: "auto"}}>
+        <Image />
+    </div>
+    <div style={{height: 20}}></div>
+    <div 
+        className="container"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "nowrap",
+          justifyContent: "center",
+          alignItems: "stretch",
+          alignContent: "center"
+
+        }}>
+          <img src={ Heroku } style={{height: 48}} />
+          <img src={ GOL } style={{height: 48}} />
+          <img src={ Docker } style={{height: 48}} />
+          <img src={ Alpine } style={{height: 48}} />
+          <img src={ BS } style={{height: 48}} />
+          <img src={ Html } style={{height: 48}} />
+      </div>
 
 
-
+    <div style={{height: 50}}></div>
     <Link to="/">Go Back To Home Page</Link>
     <p>
       <Link to="/mediacenter">Go To MediaCenter Page</Link>
--- src/pages/deployments.js
+++ src/pages/deployments.js
@@ -1,9 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
+import GOL from '../images/golang.jpg'
+import Alpine from '../images/alpine.png'
+import Docker from '../images/docker.png'
+import BS from '../images/bootstrap.png'
+import Html from '../images/html.png'
+import AWS from '../images/aws.png'
+import Heroku from '../images/heroku.jpg'
 
 const DeploymentsPage = () => (
   <Layout>
@@ -11,41 +16,64 @@ const DeploymentsPage = () => (
     <h1 id="P2">Cloud Deployments</h1>
     
   
-    <p>These are my Cloud deployments.</p>
+    <p>To make myself familiar with the Cloud ecosystem, I have
+      deployed several sites with an assortment of cloud providers.
+    </p>
     <p>
         <h2>AWS Cloud Services</h2>
         <Link to="http:/54.200.123.91:9191/">
-            
-                MyBlog
-            
-        </Link>
-
-    </p>
-    <p>
-        <h2>Huroku Cloud Services</h2>
-        <Link to="http:/54.200.123.91:9191/">
-           
-                AlpaTree
-            
+          MyBlog
         </Link>
         <p style={{marginTop: 20}}>
-            <h4>Tech:</h4>
-            Golang, Bootstrap 4, HTML
+        <div 
+          className="container"
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "nowrap",
+            justifyContent: "center",
+            alignItems: "stretch",
+            alignContent: "center"
 
+          }}>
+          <img src={ AWS } style={{height: 60}}></img>
+          <img src={ GOL } style={{height: 60}}></img>
+          <img src={ Docker } style={{height: 60}}></img>
+          <img src={ Alpine } style={{height: 60}}></img>
+          <img src={ BS } style={{height: 60}}></img>
+          <img src={ Html } style={{height: 60}}></img>
+        </div>
         </p>
-
-
-      
-
-
-
     </p>
+    <p>
+        <h2>Heroku Cloud Services</h2>
+        <Link to="http:/54.200.123.91:9191/">
+          AlpaTree
+        </Link>
+    </p>
+    <p style={{marginTop: 20}}>
+      
+      <div 
+        className="container"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "nowrap",
+          justifyContent: "center",
+          alignItems: "stretch",
+          alignContent: "center"
 
-
-
-
+        }}>
+          <img src={ Heroku } style={{height: 60}}></img>
+          <img src={ GOL } style={{height: 60}}></img>
+          <img src={ Docker } style={{height: 60}}></img>
+          <img src={ Alpine } style={{height: 60}}></img>
+          <img src={ BS } style={{height: 60}}></img>
+          <img src={ Html } style={{height: 60}}></img>
+      </div>
+    </p>
     <div style={{height: 30}}></div>
-    <Link to="/mediacenter/">Go Back To MediaCenter Page</Link>
+    <Link to="/alphatree/">Go Back To AlphaTree Page</Link>
     <p>
         <Link to="/">Go To Home Page</Link>
     </p>
