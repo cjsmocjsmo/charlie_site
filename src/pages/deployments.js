b22fba4de828b0ed3a11907f0e56130090bff532
Charlie <porthose.cjsmo@gmail.com>
2020-01-24 05:40:15 -0800

    made new logo component

--- src/pages/deployments.js
+++ src/pages/deployments.js
@@ -2,14 +2,8 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import GOL from '../images/golang.jpg'
-import Alpine from '../images/alpine.png'
-import Docker from '../images/docker.png'
-import BS from '../images/bootstrap.png'
-import Html from '../images/html.png'
-import AWS from '../images/aws.png'
-import Heroku from '../images/heroku.jpg'
-
+import AWSDepLogo from '../components/AWSDepLogo'
+import HerokuDepLogo from '../components/HerokuDepLogo'
 const DeploymentsPage = () => (
   <Layout>
     <SEO title="Deployments Page" />
@@ -25,24 +19,7 @@ const DeploymentsPage = () => (
           MyBlog
         </Link>
         <p style={{marginTop: 20}}>
-        <div 
-          className="container"
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            flexWrap: "nowrap",
-            justifyContent: "center",
-            alignItems: "stretch",
-            alignContent: "center"
-
-          }}>
-          <img src={ AWS } style={{height: 60}}></img>
-          <img src={ GOL } style={{height: 60}}></img>
-          <img src={ Docker } style={{height: 60}}></img>
-          <img src={ Alpine } style={{height: 60}}></img>
-          <img src={ BS } style={{height: 60}}></img>
-          <img src={ Html } style={{height: 60}}></img>
-        </div>
+          <AWSDepLogo />
         </p>
     </p>
     <p>
@@ -52,25 +29,7 @@ const DeploymentsPage = () => (
         </Link>
     </p>
     <p style={{marginTop: 20}}>
-      
-      <div 
-        className="container"
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          flexWrap: "nowrap",
-          justifyContent: "center",
-          alignItems: "stretch",
-          alignContent: "center"
-
-        }}>
-          <img src={ Heroku } style={{height: 60}}></img>
-          <img src={ GOL } style={{height: 60}}></img>
-          <img src={ Docker } style={{height: 60}}></img>
-          <img src={ Alpine } style={{height: 60}}></img>
-          <img src={ BS } style={{height: 60}}></img>
-          <img src={ Html } style={{height: 60}}></img>
-      </div>
+      <HerokuDepLogo />
     </p>
     <div style={{height: 30}}></div>
     <Link to="/alphatree/">Go Back To AlphaTree Page</Link>
