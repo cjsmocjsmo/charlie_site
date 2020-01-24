fcee8b7c8c45bdbb38f4c97b9c7e4002ea8e643c
Charlie <porthose.cjsmo@gmail.com>
2020-01-24 05:38:55 -0800

    added new components

---
+++ src/components/AWSDepLogo.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import GOL from '../images/golang.jpg'
+import Alpine from '../images/alpine.png'
+import Docker from '../images/docker.png'
+import BS from '../images/bootstrap.png'
+import Html from '../images/html.png'
+import AWS from '../images/aws.png'
+
+const AWSDepLogo = () => (
+    <div 
+        className="container"
+        style={{
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "nowrap",
+        justifyContent: "center",
+        alignItems: "stretch",
+        alignContent: "center"
+    }}>
+    <img src={ AWS } style={{height: 48}} alt="" />
+    <img src={ GOL } style={{height: 48}} alt="" />
+    <img src={ Docker } style={{height: 48}} alt="" />
+    <img src={ Alpine } style={{height: 48}} alt="" />
+    <img src={ BS } style={{height: 48}} alt="" />
+    <img src={ Html } style={{height: 48}} alt="" />
+  </div>
+
+)
+
+export default AWSDepLogo
---
+++ src/components/MediaCenterTech.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Docker from '../images/docker.png'
+import Html from '../images/html.png'
+import BS from '../images/bootstrap.png'
+
+
+import Alpine from '../images/alpine.png'
+import MDB from '../images/mongodb.png'
+import GL from '../images/golang.jpg'
+
+
+
+
+
+const MediaCenterTech = () => (
+  <div 
+      className="container"
+      style={{
+      display: "flex",
+      flexDirection: "row",
+      flexWrap: "nowrap",
+      justifyContent: "center",
+      alignItems: "stretch",
+      alignContent: "center",
+    }}>
+    <img src={ Html } style={{height: 48}} alt=""/>
+    <img src={ BS } style={{height: 48}} alt=""/>
+    <img src={ MDB } style={{height: 48}} alt=""/>
+    <img src={ Alpine } style={{height: 48}} alt=""/>
+    <img src={ Docker } style={{height: 48}} alt=""/>
+    <img src={ GL } style={{height: 48}} alt=""/>
+  </div>
+
+)
+export default MediaCenterTech
