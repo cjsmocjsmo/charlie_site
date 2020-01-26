aa7781cea5d7fda38e36efa50e04845eff4a6bbb
Charlie <porthose.cjsmo@gmail.com>
2020-01-25 20:37:57 -0800

    completed alphatree

--- src/pages/alphatree.js
+++ src/pages/alphatree.js
@@ -8,28 +8,24 @@ import HerokuDepLogo from '../components/HerokuDepLogo'
 const AlphaTreePage = () => (
   <Layout>
     <SEO title="AlphaTree Page" />
-
     <h1 id="P2">AlphaTree Service</h1>
     <p style={{marginTop: 30}}>
       AlphaTree is designed to give its owner a web presence, and to 
       mainly generate telephone leads.
     </p>
-    <p> The website was built using HTML5
-      and the BootStrap CSS framework. Golang was used to write the HTTP 
-      server and a 
-
-      was used to create the 17MB container image that is being
-      hosted on Heroku Cloud Services
-    </p>
     <div style={{maxWidth: 400, margin: "auto"}}>
       <AlphaTreeImage />
     </div>
     <div style={{height: 30}}></div>
+    <p> The website was built using HTML5
+      and the BootStrap CSS framework. Golang was used to write the HTTP 
+      server and a was used to create the 17MB container image that is 
+      being hosted on Heroku Cloud Services
+    </p>
     <HerokuDepLogo />
     <div style={{height: 30}}></div>
-    {/* <Link to="/deployments/">Go Back To Deployments Page</Link> */}
     <p>
-        <Link to="/deployments/">Go To Deployment Page</Link>
+      <Link to="/deployments/">Go To Deployment Page</Link>
     </p>
   </Layout>
 )
