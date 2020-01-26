5d1c188d0a6900197c7809eca15c8f72a78f99ce
Charlie <porthose.cjsmo@gmail.com>
2020-01-25 19:41:39 -0800

    increased body width to fit entire screen for larger screen sizes

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,8 +29,12 @@ const Layout = ({ children }) => {
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
+          maxWidth: 1375,
           padding: `0 1.0875rem 1.45rem`,
+
+          backgroundColor: "black",
+          color: "white"
+          
         }}
       >
         <main>{children}</main>
